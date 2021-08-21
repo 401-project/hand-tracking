3eed7f5a5a0292250609c05ef1271c35ae1fc3e6
AbdullahELian1 <abodelian28@gmail.com>
2021-08-21 16:52:01 +0300

    hand deduction: get the position for the landmark

--- hand_tracking/hand_tracking/hand_tracking.py
+++ hand_tracking/hand_tracking/hand_tracking.py
@@ -19,7 +19,19 @@ while True:
     
     if results.multi_hand_landmarks:
         for hand_lms in results.multi_hand_landmarks:
+            for id,lm_position in enumerate(hand_lms.landmark):
+                # print(id,lm_position)
+
+                high,width,channel= img.shape
+                cx,cy= int(lm_position.x*width), int(lm_position.y*high)
+                print(cx,cy)
+                
+
+                if id==4:
+                    cv2.circle(img,(cx,cy),25,(255,255,255),cv2.FILLED)
+
             mp_draw.draw_landmarks(img, hand_lms , mp_hands.HAND_CONNECTIONS)
+
 
     c_time=time.time()
     fps=1/(c_time - p_time)
