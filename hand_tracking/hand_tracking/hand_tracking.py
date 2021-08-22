713ff0e4ed062e31d2239e62aff86b902db45e62
khaled al shishani <khaled.shishani32@gmail.com>
2021-08-22 15:17:40 +0300

    hand detection: refact the code by add find_hand  method

--- hand_tracking/hand_tracking/hand_tracking.py
+++ hand_tracking/hand_tracking/hand_tracking.py
@@ -18,25 +18,53 @@ class HandDetection:
         self.mp_draw= mp.solutions.drawing_utils
 
 
-cap=cv2.VideoCapture(0)
-p_time=0
-c_time=0
-while True:
-    success , img = cap.read()
-    img_RBG= cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
-    results= hands.process(img_RBG)
-    
-    if results.multi_hand_landmarks:
-        for hand_lms in results.multi_hand_landmarks:
-            mp_draw.draw_landmarks(img, hand_lms , mp_hands.HAND_CONNECTIONS)
+def find_hand(self,img,draw=True):
 
-    c_time=time.time()
-    fps=1/(c_time - p_time)
-    p_time=c_time
+        img_RBG= cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+        results= self.hands.process(img_RBG)
+        
+        if results.multi_hand_landmarks:
+            for hand_lms in results.multi_hand_landmarks:
+                if draw:
+                    self.mp_draw.draw_landmarks(img, hand_lms , self.mp_hands.HAND_CONNECTIONS)
+        
+        return img
 
-    cv2.putText(img,str(int(fps)),(10,70),cv2.FONT_HERSHEY_COMPLEX,3,(255,0,0),3)
+
+                
+
+def main():
+    cap=cv2.VideoCapture(0)
+    p_time=0
+    c_time=0
+    checkhand=HandDetection()
+    while True:
+        success , img = cap.read()
+        img =checkhand.find_hand(img)
+
+        c_time=time.time()
+        fps=1/(c_time - p_time)
+        p_time=c_time
+
+        cv2.putText(img,str(int(fps)),(10,70),cv2.FONT_HERSHEY_COMPLEX,3,(255,0,0),3)
+        
+        
+        cv2.imshow("Image",img)
+        cv2.waitKey(1)
     
-    
-    cv2.imshow("Image",img)
-    cv2.waitKey(1)
+if __name__=="__main__":
+    main()
+
+
+
+
+
+
+
+
+
+
+
+
+
     
