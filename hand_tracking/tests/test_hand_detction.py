d0ba4ea0b693f59186d78022461527b358b64ce8
AbdullahELian1 <abodelian28@gmail.com>
2021-08-23 13:27:51 +0300

    fix action on github

--- hand_tracking/tests/test_hand_detction.py
+++ hand_tracking/tests/test_hand_detction.py
@@ -1,18 +1,18 @@
 import cv2
-from hand_tracking.hand_tracking import *
+from hand_tracking.hand_tracking.hand_tracking import *
 def test_camera_working():
     cap=cv2.VideoCapture(0)
     assert cap
 
 def test_detection_hand():
     test=HandDetection()
-    img = cv2.imread("hand_tracking/assest/hand.jpg")
+    img = cv2.imread("hand_tracking/hand_tracking/assest/hand.jpg")
     test.find_hand(img)
     assert test.check_hand    
 
 def test_target_position():
     test=HandDetection()
-    img = cv2.imread("hand_tracking/assest/hand.jpg")
+    img = cv2.imread("hand_tracking/hand_tracking/assest/hand.jpg")
     test.find_hand(img)
     test.position(img)
     assert test.check_position    
