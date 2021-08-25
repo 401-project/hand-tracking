e0a8ba07ed29b711eda43da172cb19b402308311
ahmadabudames <ahmadabudamesmais1999@gmail.com>
2021-08-25 12:53:33 +0300

    draw for unit test

--- hand_tracking/tests/test_hand_detction.py
+++ hand_tracking/tests/test_hand_detction.py
@@ -18,3 +18,44 @@ def test_target_position():
     test.position(img)
     assert test.check_position    
  
+def test_no_detection_hand():
+    test=HandDetection()
+    img = cv2.imread("hand_tracking/hand_tracking/assest/logo.jpeg")
+    test.find_hand(img)
+    assert test.check_hand  == False  
+
+def test_no_target_position():
+    test=HandDetection()
+    img = cv2.imread("hand_tracking/hand_tracking/assest/logo.jpeg")
+    test.find_hand(img)
+    test.position(img)
+    assert test.check_position  == False   
+    
+def test_fingers_up():
+    test=HandDetection()
+    img = cv2.imread("hand_tracking/hand_tracking/assest/hand2.jpg")
+    test.find_hand(img)
+    test.position(img)
+    actual =test.find_finger_up()
+    expected = [1, 1, 1, 1, 1]
+    assert actual == expected   
+    
+
+def test_one_finger_up():
+    test=HandDetection()
+    img = cv2.imread("hand_tracking/hand_tracking/assest/one_finger_up.png")
+    test.find_hand(img)
+    test.position(img)
+    actual =test.find_finger_up()
+    expected = [0, 1, 0, 0, 0]
+    assert actual == expected  
+    
+
+def test_two_fingers_up():
+    test=HandDetection()
+    img = cv2.imread("hand_tracking/hand_tracking/assest/two_fingers.png")
+    test.find_hand(img)
+    test.position(img)
+    actual =test.find_finger_up()
+    expected = [0, 1, 1, 0, 0]
+    assert actual == expected   
